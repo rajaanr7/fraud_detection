f964cbe555a91a333d9f4a4ec1deceb817765e75
rajaanr7 <raja_anr7@outlook.com>
2022-07-30 17:06:55 +0530

    Revert "wrong function added"
    This reverts commit cd77f978b90f9427b8b2ad7ebfeaa5821132055a.

--- data_processing.py
+++ data_processing.py
@@ -1,13 +1,1 @@
-from re import A
-
-
 print("adding all data processing tooll for data processing")
-
-a = 5
-b=7
-
-def sum(a,b):
-    c= a+b
-    return a
-
-print("added a new function which is not correct")
