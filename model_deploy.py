6d8daccc84159835409feaf0ec3d20fb34915271
rajaanr7 <raja_anr7@outlook.com>
2022-07-31 10:52:04 +0530

    model selected

---
+++ model_deploy.py
@@ -0,0 +1,13 @@
+print("Building modely for machine learning deployment")
+
+def model1():
+    print("model 1")
+
+
+def model2():
+    print("model 2")
+
+def model3():
+    print("model 3")
+
+print("Model2 is selected")
